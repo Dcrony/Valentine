1fdd16d09a8e90dd473640f99b40c93b71ba5814
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-02-06 07:47:48 +0100

    ready for production

--- valantine/src/api/axios.js
+++ valantine/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // This checks for an environment variable, otherwise defaults to local
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 export default API;
